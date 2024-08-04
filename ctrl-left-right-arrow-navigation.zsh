2daab371b8fd0765892e3bdf61aa6fa9721b1fe7
Frank Lan <franklan118@gmail.com>
2024-08-04 00:43:43 +0000

    Create ctrl-left-right-arrow-navigation.zsh

---
+++ ctrl-left-right-arrow-navigation.zsh
@@ -0,0 +1,11 @@
+# Upstream: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
+
+# [Ctrl-RightArrow] - move forward one word
+bindkey -M emacs '^[[1;5C' forward-word
+bindkey -M viins '^[[1;5C' forward-word
+bindkey -M vicmd '^[[1;5C' forward-word
+
+# [Ctrl-LeftArrow] - move backward one word
+bindkey -M emacs '^[[1;5D' backward-word
+bindkey -M viins '^[[1;5D' backward-word
+bindkey -M vicmd '^[[1;5D' backward-word
