b78437a85c6c98f265a729b366c428080f7d23ae
Frank Lan <franklan118@gmail.com>
2024-08-04 00:40:26 +0000

    Create large-history.zsh

---
+++ large-history.zsh
@@ -0,0 +1,13 @@
+# Upstream: https://github.com/bamos/zsh-history-analysis/tree/master?tab=readme-ov-file#increasing-the-history-file-size
+
+export HISTSIZE=1000000000
+export SAVEHIST=$HISTSIZE
+  
+setopt EXTENDED_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_BEEP
