97936ad06ced7cb11e179903fcdbfbb26b23d098
Frank Lan <franklan118@gmail.com>
2024-08-04 00:45:23 +0000

    Create page-up-down-navigation.zsh

---
+++ page-up-down-navigation.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+# Upstream: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
+
+# [PageUp] - Up a line of history
+if [[ -n "${terminfo[kpp]}" ]]; then
+  bindkey -M emacs "${terminfo[kpp]}" up-line-or-history
+  bindkey -M viins "${terminfo[kpp]}" up-line-or-history
+  bindkey -M vicmd "${terminfo[kpp]}" up-line-or-history
+fi
+
+# [PageDown] - Down a line of history
+if [[ -n "${terminfo[knp]}" ]]; then
+  bindkey -M emacs "${terminfo[knp]}" down-line-or-history
+  bindkey -M viins "${terminfo[knp]}" down-line-or-history
+  bindkey -M vicmd "${terminfo[knp]}" down-line-or-history
+fi
