a940462fb0b5df29fc28576f91fbfd063ce0b7fd
Frank Lan <franklan118@gmail.com>
2024-08-06 03:25:43 +0000

    Update home-end-naviation.zsh

--- home-end-naviation.zsh
+++ home-end-naviation.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+
 ## Reference: https://stackoverflow.com/questions/8638012/fix-key-settings-home-end-insert-delete-in-zshrc-when-running-zsh-in-terminat
 
 bindkey "^[[D" beginning-of-line
