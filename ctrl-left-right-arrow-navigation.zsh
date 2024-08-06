ee056371b16cb8141e5b48af2c5960653043ff6c
Frank Lan <franklan118@gmail.com>
2024-08-06 03:25:58 +0000

    Update ctrl-left-right-arrow-navigation.zsh

--- ctrl-left-right-arrow-navigation.zsh
+++ ctrl-left-right-arrow-navigation.zsh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
-# Upstream: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
 
-# [Ctrl-RightArrow] - move forward one word
+## Upstream: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
+
+## [Ctrl-RightArrow] - move forward one word
 bindkey -M emacs '^[[1;5C' forward-word
 bindkey -M viins '^[[1;5C' forward-word
 bindkey -M vicmd '^[[1;5C' forward-word
 
-# [Ctrl-LeftArrow] - move backward one word
+## [Ctrl-LeftArrow] - move backward one word
 bindkey -M emacs '^[[1;5D' backward-word
 bindkey -M viins '^[[1;5D' backward-word
 bindkey -M vicmd '^[[1;5D' backward-word
