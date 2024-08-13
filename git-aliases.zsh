56a8cf2e0bab13d27daf2fea5c884bcadf20ddbe
Frank Lan <franklan118@gmail.com>
2024-08-13 11:48:18 +1000

    Update git-aliases.zsh

--- git-aliases.zsh
+++ git-aliases.zsh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh
 
-git remote set-branches origin '*'
-
-alias gc1="git clone --depth 1 --filter=blob:none --recurse-submodules -j\"$(nproc)\" --remote-submodules"
 alias git-delete-all-branches="git for-each-ref --format '%(refname:short)' refs/heads | grep -v 'master|main' | xargs git branch -D"
+
+function gc1 {
+  git clone --depth 1 --filter=blob:none --recurse-submodules -j"$(nproc)" --remote-submodules "$1"
+}
 
 function git-change-author-frank {
   local name="Frank Lan"
