baab6e75fa0e0fe789369715e6c15f678259664f
Frank Lan <franklan118@gmail.com>
2024-08-15 14:55:38 +1000

    Update git-aliases.zsh

--- git-aliases.zsh
+++ git-aliases.zsh
@@ -2,7 +2,7 @@
 
 alias git-amend-all="git add . && git commit --amend -C HEAD"
 
-function git-delete-all-branches {
+function git-delete-all-local-branches {
   git for-each-ref --format '%(refname:short)' refs/heads \
     | grep -v 'master|main' \
     | xargs git branch -D
