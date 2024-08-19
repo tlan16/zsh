8d4890b46a3aac9b1eb045a333230e19968d4ce5
Frank Lan <franklan118@gmail.com>
2024-08-19 10:07:00 +1000

    Update docker.zsh

--- docker.zsh
+++ docker.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 function docker-kill-all {
+  # shellcheck disable=SC2046
   docker kill $(docker container ls -q) > /dev/null 2>&1 || true
   docker container prune --force > /dev/null 2>&1 || true
 }
