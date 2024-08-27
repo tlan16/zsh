8d63c46fd6e93e8d007c0c1d8f7ac25922f58119
Frank Lan <franklan118@gmail.com>
2024-08-27 12:59:48 +1000

    Update git-archiver.zsh

--- git-archiver.zsh
+++ git-archiver.zsh
@@ -2,7 +2,7 @@
 
 function init-git-archiver {
   mkdir -p scripts
-  wget https://raw.githubusercontent.com/tlan16/audio-archiver-template/main/scripts/git-adder.sh --output-document scripts/git-adder.sh
+  wget 'https://raw.githubusercontent.com/tlan16/audio-archiver-template/main/scripts/git-adder.sh' --output-document scripts/git-adder.sh
   chmod +x scripts/git-adder.sh
   git add scripts/git-adder.sh
   git commit -m 'add git adder script
