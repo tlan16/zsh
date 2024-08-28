86614aceb17f89886cb9b3adfa25084ee9fa7d4a
Frank Lan <franklan118@gmail.com>
2024-08-28 15:15:43 +1000

    Update ban-DS_Store.zsh

--- ban-DS_Store.zsh
+++ ban-DS_Store.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true > /dev/null 2>&1 &
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true > /dev/null 2>&1 &
+(&>/dev/null defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true &)
+(&>/dev/null defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true &)
