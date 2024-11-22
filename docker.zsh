1158f7617c3f5d461bc3c330fe529a645c622223
Frank Lan <franklan118@gmail.com>
2024-11-22 11:19:26 +1100

    Update docker.zsh

--- docker.zsh
+++ docker.zsh
@@ -4,6 +4,7 @@ function docker-kill-all {
   # shellcheck disable=SC2046
   docker kill $(docker container ls -q) > /dev/null 2>&1 || true
   docker container prune --force > /dev/null 2>&1 || true
+  docker volume prune --force > /dev/null 2>&1 || true
 }
 
 if [ -d "$HOME/.docker/bin" ] ; then
