d9fe9cebf6284b1c2266fd372573d05a6fc7bc3e
Frank Lan <franklan118@gmail.com>
2025-02-17 03:00:09 +0000

    Update ban-DS_Store.zsh

--- ban-DS_Store.zsh
+++ ban-DS_Store.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-if [ "$(uname)" == "Darwin" ]; then
+f [ "$(uname)" = "Darwin" ]; then
   (&>/dev/null defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true &)
   (&>/dev/null defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true &)
 fi
