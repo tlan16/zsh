bebcfb2bbaad2afc920bae83ba18be756e0dcc31
Frank Lan <franklan118@gmail.com>
2025-02-17 02:58:37 +0000

    Update free.sh

--- free.sh
+++ free.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
   function free() {
     vm_stat | perl -ne '/page size of (\d+)/ and $size=$1; /Pages\s+([^:]+)[^\d]+(\d+)/ and printf("%-16s % 16.2f Mi\n", "$1:", $2 * $size / 1048576);'
   }
