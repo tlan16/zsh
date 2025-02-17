5e906363c9d869f89fcb51ad0c51d82f8f4d3250
Frank Lan <franklan118@gmail.com>
2025-02-17 03:02:40 +0000

    Update pdf.zsh

--- pdf.zsh
+++ pdf.zsh
@@ -2,15 +2,19 @@
 
 function unlock_pdf() {
   local temp_file
-  for input_file in "$@"; do
-    temp_file=$(mktemp)
-    if [ ! -f "$input_file" ]; then
-      echo "Skipped not existing file ${input_file}"
-      continue
-    fi
-    printf "Unlocking %s ..." "$input_file"
-    qpdf --decrypt "$input_file" "$temp_file"
-    mv "$temp_file" "$input_file"
-    printf " done\n"
-  done
+  if type "qpdf" > /dev/null; then
+    for input_file in "$@"; do
+      temp_file=$(mktemp)
+      if [ ! -f "$input_file" ]; then
+        echo "Skipped not existing file ${input_file}"
+        continue
+      fi
+      printf "Unlocking %s ..." "$input_file"
+      qpdf --decrypt "$input_file" "$temp_file"
+      mv "$temp_file" "$input_file"
+      printf " done\n"
+    done
+  else
+    echo "pqdf is not installed"
+  fi
 }
