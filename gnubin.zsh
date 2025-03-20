c2349fe3447913ac4f1556643e2416119915dafb
Frank Lan <franklan118@gmail.com>
2025-03-20 22:41:34 +0000

    Update gnubin.zsh

--- gnubin.zsh
+++ gnubin.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+# coreutils
 if [ -d "/opt/homebrew/opt/coreutils/libexec/gnubin" ] ; then
   export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+fi
+
+# uutils-coreutils
+if [ -d "/opt/homebrew/opt/uutils-coreutils/libexec/uubin" ] ; then
+  export PATH="/opt/homebrew/opt/uutils-coreutils/libexec/uubin:$PATH"
 fi
