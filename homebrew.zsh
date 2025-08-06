56be4a42c3a26612471a6e26831005794d5dad8e
Frank Lan <franklan118@gmail.com>
2025-08-07 09:46:42 +1000

    Update homebrew.zsh

--- homebrew.zsh
+++ homebrew.zsh
@@ -1,22 +1,20 @@
 #!/usr/bin/env zsh
 
-if [ "$(uname)" = "Darwin" ]; then
-  function brew-cleanup-all {
-    brew autoremove
-    brew cleanup --prune=all
-  }
-  
-  if [ -d "/opt/homebrew/bin" ] ; then
-    export PATH="/opt/homebrew/bin:$PATH"
-  fi
-  if [ -d "/opt/homebrew/sbin" ] ; then
-    export PATH="/opt/homebrew/sbin:$PATH"
-  fi
-  
-  if [ -d "/opt/homebrew" ] ; then
-    alias brew-install-from-source="brew install --build-from-source"
-    alias brew-outdated-formula="brew outdated --formula"
-    alias brew-upgrade-all-formula='brew outdated --formulae | xargs -I{} brew reinstall --build-from-source {}'
-    alias brew-remove-lock="$(brew --prefix)/var/homebrew/locks"
-  fi
+function brew-cleanup-all {
+  brew autoremove
+  brew cleanup --prune=all
+}
+
+if [ -d "/opt/homebrew/bin" ] ; then
+  export PATH="/opt/homebrew/bin:$PATH"
+fi
+if [ -d "/opt/homebrew/sbin" ] ; then
+  export PATH="/opt/homebrew/sbin:$PATH"
+fi
+
+if [ -d "/opt/homebrew" ] ; then
+  alias brew-install-from-source="brew install --build-from-source"
+  alias brew-outdated-formula="brew outdated --formula"
+  alias brew-upgrade-all-formula='brew outdated --formulae | xargs -I{} brew reinstall --build-from-source {}'
+  alias brew-remove-lock="$(brew --prefix)/var/homebrew/locks"
 fi
